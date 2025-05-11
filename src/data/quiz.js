ddec3a80495680ea14f2e6cf6cccb4aa82f8aca3
isshiki <mono.color25@gmail.com>
2025-05-11 17:16:40 +0900

    add:問題文の型

---
+++ src/data/quiz.js
@@ -0,0 +1,29 @@
+const quizData= [
+    {
+        question: "テキスト",
+        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+        answerIndex: 0
+    },
+    {
+        question: "テキスト2",
+        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+        answerIndex: 0
+    },
+    {
+        question: "テキスト3",
+        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+        answerIndex: 0
+    },
+    {
+        question: "テキスト4",
+        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+        answerIndex: 0
+    },
+    {
+        question: "テキスト5",
+        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
+        answerIndex: 0
+    },
+];
+
+export default quizData;
