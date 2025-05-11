a53a3e856b88c8a1d8909da328588598f3d9043a
isshiki <mono.color25@gmail.com>
2025-05-11 18:13:06 +0900

    add:staticクイズページ選択肢の表示

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,13 +1,27 @@
+import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
 
 export default function QuizPage() {
     const quizIndex = 0;
+    const handleClick = (clickedIndex) =>{
+        console.log('clickIndex', clickedIndex);
+    }
   return (
     <>
         <Display>
             {`Q1. ${quizData[quizIndex].question}`}
         </Display>
+        {
+            quizData[quizIndex].options.map((option, index)=>{
+                let optNum = index + 1;
+                return (
+                    <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+                        {optNum} . {option}
+                    </Button>
+                )
+            })
+        }
     </>
   )
   
