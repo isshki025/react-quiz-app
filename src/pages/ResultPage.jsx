b75e45311d615b88c04f4af279764eb4af6b06f4
isshiki <mono.color25@gmail.com>
2025-05-12 15:36:54 +0900

    add:結果ページの作成

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,17 @@
+import { Link, useLocation } from "react-router-dom"
+import { ROUTES } from "../const";
+
 export default function ResultPage() {
+  const location = useLocation();
+  const maxQuizLen = location.state.maxQuizLen;
+  const correctCountLen = location.state.correctCountLen;
+
   return (
-    <div>ResultPage</div>
+    <>
+      <h1>結果発表</h1>
+      <p>あなたの正解数は</p>
+      <p>{`全${maxQuizLen}問中、${correctCountLen}問正解でした`}</p>
+        <Link to={ROUTES.QUIZ}>もう一度挑戦する</Link>
+    </>
   )
 }
